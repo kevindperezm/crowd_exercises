136e204184d32b3e2de1c9f004eaa08192a821fe
Kevin Perez <kevindperezm@gmail.com>
2014-05-28 18:41:18 -0500

    Change 'word' to 'string'

--- palindromes/spec/lib/validators/repetition_count_validator_spec.rb
+++ palindromes/spec/lib/validators/repetition_count_validator_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 
 describe RepetitionCountValidator do
   context '#validate' do
-    it 'returns true if even-length word repeats each char even times' do
+    it 'returns true if even-length string repeats each char even times' do
       expect(subject.validate 'atgagaaddteaetuaut').to be_true
     end
-    it('returns false if even-length word repeats a char odd times') do
+    it('returns false if even-length string repeats a char odd times') do
       expect(subject.validate 'gxagaddeetuuvt').to be_false
     end
-    it 'returns true if odd-length word repeats each repeated char even times' \
+    it 'returns true if odd-length string repeats each repeated char even times' \
     do
       expect(subject.validate 'ggggaaaattrrrrrry').to be_true
     end
-    it 'returns false if odd-length word repeats a repeated char odd times' \
+    it 'returns false if odd-length string repeats a repeated char odd times' \
     do
       expect(subject.validate 'ggggaaaattrrrrrrt').to be_false
     end
