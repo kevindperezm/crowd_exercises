901af1d0289ee17eb5e76755d46b69b3d9865976
Kevin Perez <kevindperezm@gmail.com>
2014-05-28 19:11:12 -0500

    First try to integrate all

---
+++ palindromes/lib/palindrome_generator.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+# Creates palindromes from string's chars of input file
+class PalindromeGenerator
+  def initialize(input_path, output, validators)
+    @input_file = File.open input_path
+    @output = output
+    @validators = validators
+  end
+
+  def run
+    lines_to_read = @input_file.readline.chomp.to_i
+    @output.open
+    lines_to_read.times do
+      @output.write(validate?(@input_file.readline.chomp) ? "yes!\n" : "no!\n")
+    end
+    @output.close
+  end
+
+  def validate?(string)
+    @validators.all? { |validator| validator.validate?(string) }
+  end
+end
