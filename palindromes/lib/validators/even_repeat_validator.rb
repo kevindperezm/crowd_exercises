b36474af32cf5c2292234ff37f388905921506d0
Kevin Perez <kevindperezm@gmail.com>
2014-05-28 19:58:48 -0500

    Add comments with considered exceptions

--- palindromes/lib/validators/even_repeat_validator.rb
+++ palindromes/lib/validators/even_repeat_validator.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 
 # Matcher for rule 'Repeat count for every char repetition is multiple of 2'
+# This class also consider the next exceptions in its logic:
+# * In case of odd-size string, there can be 1 and just 1 isolated char.
+# * The isolated char can be one of the repeated chars.
 class EvenRepeatValidator
   def validate?(string)
     string.chars.all? { |char| check_char?(string, char) } ||
