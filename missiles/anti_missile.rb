9a018764b5d04aa6fe9e56c066142dba46994634
Kevin Perez <kevindperezm@gmail.com>
2014-06-11 18:36:24 -0500

    receives input dir as first argument

--- missiles/anti_missile.rb
+++ missiles/anti_missile.rb
@@ -1,3 +1,5 @@
+#! /usr/bin/env ruby
+
 require './lib/binary_tree/binary_tree_node'
 require './lib/strategies/prune_strategy'
 require './lib/input/file_input'
@@ -5,11 +7,23 @@ require './lib/output/file_output'
 
 INPUT_FILE_EXTENSION = '.in'
 OUTPUT_FILE_EXTENSION = '.out'
-INPUT_DIRECTORY = '.'
 
-Dir["#{INPUT_DIRECTORY}/*#{INPUT_FILE_EXTENSION}"].each do |entry|
+def input_directory
+  input_dir = nil
+  begin
+    input_dir = ARGV[0].chomp('\\')
+    fail IOError unless File.exist?(input_dir)
+  rescue IOError
+    puts "Unreachable dir '#{input_dir}'\nExit"
+    exit
+  end
+  input_dir
+end
+
+Dir["#{input_directory}/*#{INPUT_FILE_EXTENSION}"].each do |entry|
   missiles = FileInput.new(entry).missiles
   optimal_route = PruneStrategy.new(missiles).optimal_missile_destruction_route
   basename = File.basename(entry, INPUT_FILE_EXTENSION)
-  FileOutput.new("#{basename}#{OUTPUT_FILE_EXTENSION}").write(optimal_route)
+  output_path = "#{input_directory}/#{basename}#{OUTPUT_FILE_EXTENSION}"
+  FileOutput.new("#{output_path}").write(optimal_route)
 end
