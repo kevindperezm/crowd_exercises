0c55aefcedaa2859aa4007c708d577591a9be1f4
Kevin Perez <kevindperezm@gmail.com>
2014-06-11 18:08:08 -0500

    functional app with fixed input directory

--- missiles/anti_missile.rb
+++ missiles/anti_missile.rb
@@ -1,6 +1,15 @@
 require './lib/binary_tree/binary_tree_node'
 require './lib/strategies/prune_strategy'
-require './lib/input/file_input' 
+require './lib/input/file_input'
 require './lib/output/file_output'
 
+INPUT_FILE_EXTENSION = '.in'
+OUTPUT_FILE_EXTENSION = '.out'
+INPUT_DIRECTORY = '.'
 
+Dir["#{INPUT_DIRECTORY}/*#{INPUT_FILE_EXTENSION}"].each do |entry|
+  missiles = FileInput.new(entry).missiles
+  optimal_route = PruneStrategy.new(missiles).optimal_missile_destruction_route
+  basename = File.basename(entry, INPUT_FILE_EXTENSION)
+  FileOutput.new("#{basename}#{OUTPUT_FILE_EXTENSION}").write(optimal_route)
+end
