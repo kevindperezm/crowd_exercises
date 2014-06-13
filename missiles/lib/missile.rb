ca8c896b66f12e1ea3fc5a53124d8e2bad10bb9b
Kevin Perez <kevindperezm@gmail.com>
2014-06-13 13:24:10 -0500

    missile object is now comparable

--- missiles/lib/missile.rb
+++ missiles/lib/missile.rb
@@ -1,4 +1,14 @@
 # DTO for missiles
 class Missile
+  include Comparable
   attr_accessor :position, :altitude
+
+  def initialize(position, altitude)
+    @position = position
+    @altitude = altitude
+  end
+
+  def <=>(another)
+    @altitude <=> another.altitude
+  end
 end
