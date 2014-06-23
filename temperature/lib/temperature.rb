9e2d267c7f80bf90dbae1764d4c3d8867d1adfcb
Kevin Perez <kevindperezm@gmail.com>
2014-06-23 17:32:41 -0500

    added in_celsius method

--- temperature/lib/temperature.rb
+++ temperature/lib/temperature.rb
@@ -1,3 +1,10 @@
 # Converts temperatures from Celsius to Farenheit and viceversa
 class Temperature
-end 
+  def initialize(degrees)
+    @degrees = degrees
+  end
+
+  def self.in_celsius(degrees)
+    Temperature.new(degrees)
+  end
+end
