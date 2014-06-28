e70349d50e3a76334ee3070a5940ef88ddbed933
Kevin Perez <kevindperezm@gmail.com>
2014-06-28 14:10:08 -0500

    remove conflict markers

--- missiles/lib/output/file.rb
+++ missiles/lib/output/file.rb
@@ -1,9 +1,4 @@
 module Output
-<<<<<<< HEAD
-  StandardFile = File
-
-=======
->>>>>>> make-antimissile-unaware-of-the-console
   # Outputs missile destruction route data to a file
   class File
     def initialize(path)
@@ -11,21 +6,12 @@ module Output
     end
 
     def write(missiles_route)
-<<<<<<< HEAD
-      file = StandardFile.new(@path, 'w')
-      file.write("#{missiles_route.size}\n")
-      missiles_route.each do|missile_position|
-        file.write("#{missile_position}\n")
-      end
-      file.close
-=======
       open(@path, 'w') do |f|
         f.write("#{missiles_route.size}\n")
         missiles_route.each do |missile_position|
           f.write("#{missile_position}\n")
         end
       end
->>>>>>> make-antimissile-unaware-of-the-console
     end
   end
 end
