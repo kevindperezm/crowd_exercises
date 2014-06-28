604933f7d5be126cbd21d00674a3c279467fd64b
Kevin Perez <kevindperezm@gmail.com>
2014-06-28 11:36:40 -0500

    add methods to load from and write to a file

--- missiles/lib/missile_destroyer.rb
+++ missiles/lib/missile_destroyer.rb
@@ -1,9 +1,11 @@
 require_relative 'destruction_strategies/multiple_tree'
+require_relative 'input/file'
+require_relative 'output/file'
 
 class MissileDestroyer
   attr_accessor :missiles, :strategy
 
-  def initialize(missiles, options = {})
+  def initialize(missiles = [], options = {})
     @missiles = missiles
     @strategy = options[:strategy]
   end
@@ -11,6 +13,14 @@ class MissileDestroyer
   def best_destruction_route
     @strategy ||= DestructionStrategies::MultipleTree.new(@missiles)
     @strategy.best_destruction_route
+  end
+
+  def load_missiles_from_file(path)
+    missiles = Input::File.new(path).missiles
+  end
+
+  def write_route_to_file(route, path)
+    Output::File.new(path).write(route)
   end
 
   def missiles=(missiles)
